0fbd85569d854f93942b5a41b33dcc5699a2a8e0
Benson <109269567+BensonGithubStudio@users.noreply.github.com>
2026-08-21 13:06:08 +0800

    Add files via upload

--- js/food-crud.js
+++ js/food-crud.js
@@ -206,9 +206,7 @@ function renderList(data){
             type.innerHTML = '<i class="bi bi-tag-fill"></i> ' + escapeHtml(item.type);
             // 重力垂墜角：支點在左側、標籤主體往右懸出去，重力會讓它自然往右下垂一點角度，
             // 每顆標籤給不同的角度（4~8 度），看起來才不會像是同一個模子刻出來的
-            var typeTilt = (4 + Math.random() * 4).toFixed(1);
-            type.style.setProperty("--tag-rest-tilt", typeTilt + "deg");
-            type.dataset.restTilt = typeTilt;
+            type.style.setProperty("--tag-rest-tilt", (4 + Math.random() * 4).toFixed(1) + "deg");
         }
 
         /* 地區標籤（依地址判斷，不同地區給不同顏色） */
@@ -221,9 +219,7 @@ function renderList(data){
             regionTag.style.setProperty("--tag-color", colors.color);
             regionTag.innerHTML = '<i class="bi bi-geo-alt-fill"></i> ' + escapeHtml(regionName);
             // 同上的重力垂墜角，各標籤獨立算一次，兩顆標籤的垂墜角不會一樣
-            var regionTilt = (4 + Math.random() * 4).toFixed(1);
-            regionTag.style.setProperty("--tag-rest-tilt", regionTilt + "deg");
-            regionTag.dataset.restTilt = regionTilt;
+            regionTag.style.setProperty("--tag-rest-tilt", (4 + Math.random() * 4).toFixed(1) + "deg");
         }
 
         /* 標籤列：類型 + 地區 */
